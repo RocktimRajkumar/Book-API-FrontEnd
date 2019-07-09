807dd232ccbab88d5eb5890b5c9d19a0148afa7b
Rocktim <rajkumar.rocktim@gmail.com>
2019-07-09 14:51:47 +0530

    ngx-ui-loader added and s.no in table added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookComponent } from './book/book.component';
 import { BookService } from './book/book.service';
+import { NgxUiLoaderModule, NgxUiLoaderConfig, SPINNER } from 'ngx-ui-loader';
+
+const ngxUiLoaderConfig: NgxUiLoaderConfig = {
+  fgsColor: "#10e531",
+  fgsSize: 110,
+  fgsType: SPINNER.threeStrings, // foreground spinner type
+  pbThickness: 5, // progress bar thickness
+  pbColor: "green"
+};
 
 @NgModule({
   declarations: [
@@ -17,7 +26,8 @@ import { BookService } from './book/book.service';
     BrowserModule,
     AppRoutingModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    NgxUiLoaderModule.forRoot(ngxUiLoaderConfig)
   ],
   providers: [BookService],
   bootstrap: [AppComponent]
--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from './book';
 import { BookService } from './book.service';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 
 @Component({
   selector: 'app-book',
@@ -12,26 +13,32 @@ export class BookComponent implements OnInit {
 
   books: Book[];
   book = new Book();
-  constructor(private _bookService: BookService) { }
+  constructor(private _bookService: BookService, private ngxService: NgxUiLoaderService) { }
 
   getBooks(): void {
+    this.ngxService.start();
     this._bookService.getAllBooks()
       .subscribe((bookData) => {
         this.books = bookData;
-        console.log(bookData)
+        console.log(bookData);
+        this.ngxService.stop();
       }, (error) => {
         console.log(error);
+        this.ngxService.stop();
       });
   }
 
   addBook(): void {
+    this.ngxService.start();
     this._bookService.addBook(this.book)
       .subscribe((response) => {
         console.log(response);
         this.reset();
         this.getBooks();
+        this.ngxService.stop();
       }, (error) => {
         console.log(error);
+        this.ngxService.stop();
       });
   }
 
@@ -42,23 +49,29 @@ export class BookComponent implements OnInit {
   }
 
   deleteBook(bookId: String) {
+    this.ngxService.start();
     this._bookService.deleteBook(bookId)
       .subscribe((response) => {
         console.log(response);
         this.getBooks();
+        this.ngxService.stop();
       }, (error) => {
         console.log(error);
+        this.ngxService.stop();
       });
   }
 
   getBookById(bookId: String) {
+    this.ngxService.start();
     this._bookService.getBookById(bookId)
       .subscribe((bookData)=>{
         console.log(bookData);
         this.book=bookData;
         this.getBooks();
+        this.ngxService.stop();
       },(error)=>{
         console.log(error);
+        this.ngxService.stop();
       });
   }
 
